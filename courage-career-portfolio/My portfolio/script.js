7ff28de9a15b3e018668f77b26790f5d19217d3b
Oluwapelumi Igwebuike <87787407+Pelumi2001@users.noreply.github.com>
2024-04-02 23:44:02 -0500

    Add files via upload

---
+++ courage-career-portfolio/My portfolio/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+// Get the button element
+let mybutton = document.getElementById("myBtn");
+
+// Show the button when the user scrolls down 20px from the top
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Scroll to the top of the document when the button is clicked
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
